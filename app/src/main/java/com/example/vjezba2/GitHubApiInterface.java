79f3db239602a8a38c0aec5b8af04905a66e7b2f
unknown <fico.zd@hotmail.com>
2020-10-26 12:52:23 +0100

    API interface created

---
+++ app/src/main/java/com/example/vjezba2/GitHubApiInterface.java
@@ -0,0 +1,10 @@
+package com.example.vjezba2;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+public interface GitHubApiInterface {
+    @GET("search/repositories?q=stars:>200000")
+    Call<List<GitHubData>> getData();
+}
--- app/src/main/java/com/example/vjezba2/MainActivity.java
+++ app/src/main/java/com/example/vjezba2/MainActivity.java
@@ -16,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
+
     }
 }
