58a114e5697709ccb03ab6498fdd5848b7cd56c9
unknown <fico.zd@hotmail.com>
2020-10-26 12:43:01 +0100

    Retrofit object created

--- app/src/main/java/com/example/vjezba2/MainActivity.java
+++ app/src/main/java/com/example/vjezba2/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.vjezba2;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String BASE_URL = "https://api.github.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
     }
 }
